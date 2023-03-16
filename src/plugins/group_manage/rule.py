eb63a1abbb4d845e47c0602ed05eddbc733a56ac
anda522 <wyq522208@qq.com>
2023-03-16 22:09:48 +0800

    Add rule.py

--- src/plugins/group_manage/rule.py
+++ src/plugins/group_manage/rule.py
@@ -5,11 +5,22 @@
 from nonebot.adapters.onebot.v11 import (
     Bot,
     Event,
+    GroupMessageEvent,
 )
+from nonebot.permission import Permission
+from . import approve
+from .config import plugin_config
 
 
-# 检测发送qq是否是分群管理
-async def user_checker(event: Event) -> bool:
-    for qq in group_admin:
-        if qq == Event.user_id():
-            return True
+# 检测发送信息的qq是否是分群管理
+async def user_checker(event: GroupMessageEvent) -> bool:
+    uid = event.get_user_id()
+    sub_admins = approve.get_sub_admins()
+    gid = event.group_id
+    try:
+        qq_list = sub_admins[str(gid)]
+        for qq in qq_list:
+            if uid == qq:
+                return True
+    except ValueError:
+        return False
